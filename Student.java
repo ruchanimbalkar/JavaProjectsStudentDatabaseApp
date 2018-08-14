377a17f3b3d8cae9167ff438c55f2efcfa3a6303
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2018-08-14 07:50:27 -0700

    enroll method

--- Student.java
+++ Student.java
@@ -8,15 +8,15 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses;
-	private int tuitionBalance;
+	private String courses="";
+	private int tuitionBalance = 0;
 	private int costOfCourse = 600;
-	private Scanner in =new Scanner (System.in);/*Not sure if this is a good idea*/
+	//private Scanner in =new Scanner (System.in);/*Not sure if this is a good idea*/
 	private static int id =1000;
 	//Constructor : prompt user to enter student name and year
 	public Student(){
 		
-		//Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		System.out.println(" Enter student first name : ");
 		this.firstName = in.nextLine();
 		
@@ -34,6 +34,7 @@ public class Student {
 	
 	//Generate an ID
 	private void setStudentID(){
+		
 		//Grade level + ID
 		id++;
 		this.studentID = gradeYear + "" + id;
@@ -42,7 +43,26 @@ public class Student {
 	
 	
 	//Enroll in courses
-	
+	public void enroll(){
+		
+		//Get inside loop, user hits Q to quit
+		do {
+			System.out.println("Enter course to enroll : (Q to quit)");
+		    Scanner in = new Scanner(System.in);
+		    String course = in.nextLine();
+		    if(!course.equals("Q")){
+		    	courses = courses + "\n" + course;
+		    	tuitionBalance = tuitionBalance + costOfCourse;
+			
+		    }
+		    else
+		    	break;
+		}    
+		while(1!=0);    
+		System.out.println("Enrolled in : " + courses);
+		System.out.println("Tuition Balance : " + tuitionBalance);
+		
+	}
 	//View Balance
 	
 	//Pay Tuition
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -12,6 +12,7 @@ public class StudentDatabaseApp {
 	public static void main(String[] args) {
 		
 		Student student1 = new Student();
+		student1.enroll();
 		//Ask how many new users we want to add
 		
 		
